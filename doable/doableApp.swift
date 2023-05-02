b2eefc88553c6b5dcfd90fcc1a20d52820020c92
Matheus Faleiro <bafs@novonordisk.com>
2023-05-02 11:34:30 +0200

    Initial Commit

---
+++ doable/doableApp.swift
@@ -0,0 +1,17 @@
+//
+//  doableApp.swift
+//  doable
+//
+//  Created by BAFS (Matheus Faleiro De Sousa Silva) on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct doableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
