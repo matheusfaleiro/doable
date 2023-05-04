9b529fd5d5c3b0b01cf2797ee4ae4fabb661ea4b
Matheus Faleiro <bafs@novonordisk.com>
2023-05-04 16:00:50 +0200

    Add Firebase Setup and Organise Files into Folders
    Set up the app to use Firebase for backend services by
    integrating the necessary SDK and configuring the
    FirebaseApp instance. Organize project files into
    appropriate folders for better maintainability and
    code organization. This structure will enhance
    readability and make future updates easier to manage.

--- doable/Other/doableApp.swift
+++ doable/Other/doableApp.swift
@@ -5,10 +5,15 @@
 //  Created by BAFS (Matheus Faleiro De Sousa Silva) on 02/05/2023.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct doableApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
